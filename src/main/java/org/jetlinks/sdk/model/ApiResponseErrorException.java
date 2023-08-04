bff22d655590ae7e0f2c44ac8d1088e948bdea48
gyl <1554863190@qq.com>
2023-08-04 19:01:40 +0800

    feat(设备api): 优化错误打印

--- src/main/java/org/jetlinks/sdk/model/ApiResponseErrorException.java
+++ src/main/java/org/jetlinks/sdk/model/ApiResponseErrorException.java
@@ -11,35 +11,35 @@ public class ApiResponseErrorException extends RuntimeException {
 
     private final int status;
     private final String code;
-    private final String message;
+    private final String resMessage;
     private final String timestamp;
     private final Object result;
 
     public ApiResponseErrorException(ApiResponse<Object> response) {
-        super(response.getMessage());
+        super(fullMessage(response));
         this.status = response.getStatus();
         this.code = response.getCode();
-        this.message = response.getMessage();
+        this.resMessage = response.getMessage();
         this.timestamp = response.getTimestamp();
         this.result = response.getResult();
     }
 
     public ApiResponseErrorException(ApiResponse<Object> response, String message) {
-        super(message);
+        super(fullMessage(response) + "," + message);
         this.status = response.getStatus();
         this.code = response.getCode();
-        this.message = response.getMessage();
+        this.resMessage = response.getMessage();
         this.timestamp = response.getTimestamp();
         this.result = response.getResult();
     }
 
-    public ApiResponseErrorException(ApiResponse<Object> response, String message, Throwable cause) {
-        super(message, cause);
-        this.status = response.getStatus();
-        this.code = response.getCode();
-        this.message = response.getMessage();
-        this.timestamp = response.getTimestamp();
-        this.result = response.getResult();
+
+    public static String fullMessage(ApiResponse<Object> response) {
+        return "API返回错误：code='"
+                + response.getStatus() + '\'' +
+                ", message='"
+                + response.getMessage() + '\'';
+
     }
 
 
